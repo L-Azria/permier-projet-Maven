726d7bdbc650bcac1daa1e9058713b6792ebc2cf
L-Azria <l.azria.pro@gmail.com>
2023-05-31 10:54:02 +0200

    TP2

--- src/main/java/fr/digi/m0323/App.java
+++ src/main/java/fr/digi/m0323/App.java
@@ -1,11 +1,20 @@
 package fr.digi.m0323;
-import com.github.lalyos.jfiglet.FigletFont;
 
 import java.io.IOException;
+import java.util.ResourceBundle;
+import com.github.lalyos.jfiglet.FigletFont;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        String asciiArt = FigletFont.convertOneLine("App");
+        String titre = ResourceBundle.getBundle("application").getString("titre");
+
+        String asciiArt = FigletFont.convertOneLine(titre);
         System.out.println(asciiArt);
+
+        // affichage de l'environnement
+        String environnement = ResourceBundle.getBundle("application").getString
+                ("environnement");
+        System.out.println("Environnement : " + environnement);
+
     }
 }
